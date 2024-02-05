57567880ca0da30f2b4c15a0da2ce1b20f09c3a7
Turtlewords <144561651+Turtlewords@users.noreply.github.com>
2024-02-05 13:38:32 -0600

    Finalize conversion functions in js

--- index.js
+++ index.js
@@ -5,34 +5,34 @@
 */
 
 
-const input = document.getElementById("input").value
+const input = document.getElementById("input")
 const btn = document.getElementById("btn")
 const lengthEl = document.getElementById("length-El")
 const volumeEl = document.getElementById("volume-El")
 const massEl = document.getElementById("mass-El")
 
 btn.addEventListener("click", function() {
-    convertLength(input)
-    convertVolume(input)
-    convertMass(input)
+    convertLength(input.value)
+    convertVolume(input.value)
+    convertMass(input.value)
 })
 
 
 function convertLength(input) {
     let feet = Number(input) * 3.281
     let meters = Number(input) / 3.281
-    lengthEl.innerHTML = `${input} meters = ${feet.toFixed(3)} | ${input} feet = ${meters.toFixed(3)} meters`
+    lengthEl.innerHTML = `${input} meters = ${feet.toFixed(3)} feet | ${input} feet = ${meters.toFixed(3)} meters`
 
 }
 
 function convertVolume(input) {
     let gallons = Number(input) * .264
     let liters = Number(input)  * 3.785
-    volumeEl.innerHTML = `${input} liters = ${gallons.toFixed(3)} | ${input} gallons = ${liters.toFixed(3)} liters`
+    volumeEl.innerHTML = `${input} liters = ${gallons.toFixed(3)} gallons | ${input} gallons = ${liters.toFixed(3)} liters`
 }
 
 function convertMass(input) {
     let pounds = Number(input) * 2.204
     let kilos = Number(input) / 2.204
-    volumeEl.innerHTML = `${input} kilos = ${pounds.toFixed(3)} pounds | ${input} pounds = ${kilos.toFixed(3)} kilos`
+    massEl.innerHTML = `${input} kilos = ${pounds.toFixed(3)} pounds | ${input} pounds = ${kilos.toFixed(3)} kilos`
 }
